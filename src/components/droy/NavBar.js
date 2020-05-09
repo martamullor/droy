38be0ea76eb94aeedde325b3a4be66d3efd65705
Marta Mullor <Marta@MacBook-Air-de-marta.local>
2020-05-09 20:05:00 +0200

    style NavBar done

--- src/components/droy/NavBar.js
+++ src/components/droy/NavBar.js
@@ -2,15 +2,40 @@ import React, { Component } from 'react'
 import { withData } from '../../contexts/dataContext'
 import PropTypes from 'prop-types'
 
+const navBar = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: '0px 35px',
+  backgroundColor: '#dd59da'
+}
+
+const logoMenu = {
+  width: '9%'
+}
+
+const buttonsNavBar = {
+  fontFamily: 'Montserrat, sans-serif',
+  backgroundColor: '#ef9bfd',
+  padding: '8px 30px',
+  marginRight: '10px',
+  borderRadius: '5px',
+  border: 'none',
+  color: 'white',
+  fontWeight: '600',
+  fontSize: '0.8rem'
+}
+
 class NavBar extends Component {
-  render () {
+  render() {
     const { withOptions, mode, switchMode, save } = this.props
     return (
-      <div className="nav-bar">
-        <h1>Droy</h1>
+      <div style={navBar}>
+        <img style={logoMenu} src='../../../img/logo-white.png' alt='logo-white'></img>
         {withOptions && <div>
-          <button onClick={save}>Save</button>
-          <button onClick={switchMode}>{mode === 'edit' ? 'View page' : 'Edit page'}</button></div>
+          <button style={buttonsNavBar} onClick={save}>Save</button>
+          <button style={buttonsNavBar} onClick={switchMode}>{mode === 'edit' ? 'View page' : 'Edit page'}</button></div>
         }
       </div>
     )
