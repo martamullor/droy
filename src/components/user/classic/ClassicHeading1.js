d34f721205c9902704595049fd6c599f3eadc4b4
Marc Serra <mserrahidalgo@gmail.com>
2020-05-13 22:05:07 +0200

    WIP images

--- src/components/user/classic/ClassicHeading1.js
+++ src/components/user/classic/ClassicHeading1.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Image, Transformation, CloudinaryContext } from 'cloudinary-react'
-import axios from 'axios'
+import firebase from '../../../services/firebase'
+const { uuid } = require('uuidv4');
 
 const style = {
   height: '200px',
@@ -10,23 +10,35 @@ const style = {
 
 class ClassicHeading1 extends Component {
 
-  state = { selectedFile: null }
+  state = { selectedFile: null,  image1: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png'}
 
-  fileChangedHandler = event => {
-    this.setState({ selectedFile: event.target.files[0] })
+  fileChangedHandler = e => {
+    const file = e.target.files[0]
+    // poner dentro de carpeta usuario y proyecto
+    // asi cuando borramos proyecto, borramos carpeta bucket
+    const storageRef = firebase.storage().ref(`${uuid()}`)
+    if(file.size > 20000){
+      alert('Imagen demasiado grande. Elige una mas pequeña o hazle un resize')
+      return
+    }
+    const task = storageRef.put(file)
+    task.on('state_changed', (snapshot) => {
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+      // ir enseñando progreso en front y bloquearlo de mientras
+      console.log(111, percentage)
+    }, (error) => {
+      console.error(2222, error.message)
+    }, async () => {
+      const downloadUrl = await task.snapshot.ref.getDownloadURL()
+      this.setState({image1: downloadUrl})
+    })
+    
   }
   
   uploadHandler = () => {
-    console.log(this.state.selectedFile)
+    this.refs.imageUploader.click();
   }
 
-  uploadHandler = () => {
-    axios.post('https://api.cloudinary.com/v1_1/dm99dkmlr/upload', this.state.selectedFile, {
-      onUploadProgress: progressEvent => {
-        console.log(progressEvent.loaded / progressEvent.total)
-      }
-    })
-  }
 
   render () {
     const { info, changeInfo, children: optionsBar } = this.props
@@ -34,17 +46,8 @@ class ClassicHeading1 extends Component {
 
       <div style={style}>
         { optionsBar }
-        <input type="file" onChange={this.fileChangedHandler}/>
-        <button onClick={this.uploadHandler}>Upload!</button>
-        <Image
-          dpr="auto"
-          responsive
-          responsiveUseBreakpoints="true"
-          cloudName="dm99dkmlr"
-          publicId="sample"
-          style={{ width: '100%', height: '100%' }}>
-          <Transformation effect="sepia" />
-        </Image>
+        <input style={{display: 'none'}} onChange={this.fileChangedHandler} ref="imageUploader" type="file" name="image" id="image"/>
+        <img onDoubleClick={this.uploadHandler} src={this.state.image1} alt="" style={{width: '100%', height: '100%'}}/>
         In classic heading component
         <p data-id="text1" onDoubleClick={changeInfo}>{info.text1}</p>
         <p data-id="text2" onDoubleClick={changeInfo}>{info.text2}</p>
---
+++ src/services/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase'
+
+const firebaseConfig = { // Poner estos datos en el .env
+  apiKey: 'AIzaSyBd5EjLbjtvoWmC4jKmFKcTa5Up87I9bTc',
+  authDomain: 'droy-dev.firebaseapp.com',
+  databaseURL: 'https://droy-dev.firebaseio.com',
+  projectId: 'droy-dev',
+  storageBucket: 'droy-dev.appspot.com',
+  messagingSenderId: '832956384747',
+  appId: '1:832956384747:web:3870c2e89e40d7401385ae'
+}
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
