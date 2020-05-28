4d3f6098b8019e5b7414f9a85d3a9ab2d2f5d420
Marta Mullor <Marta@MacBook-Air-de-marta.local>
2020-05-28 11:43:09 +0200

    all classic components refactored

--- src/components/user/classic/ClassicTwoContainer.js
+++ src/components/user/classic/ClassicTwoContainer.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import LinksListEditable from '../../droy/LinksListEditable'
+import LinkEditable from '../../droy/LinkEditable'
 
-const timelineContainer = {
+const twoContainer = {
   display: 'flex',
   flexDirection: 'column',
   position: 'relative',
@@ -14,10 +14,11 @@ const timelineContainer = {
 }
 
 const container4options = {
-  display: 'flex'
+  display: 'flex',
+  width: '100%'
 }
 
-const textTimelineContainerOne = {
+const textContainerOne = {
   display: 'flex',
   textAlign: 'left',
   alignItems: 'center',
@@ -26,11 +27,12 @@ const textTimelineContainerOne = {
   width: '50%',
   backgroundColor: '#97a68f',
   padding: '50px',
-  margin: '15px 30px 15px 0px',
-  borderRadius: '5px'
+  margin: '15px 50px 15px 0px',
+  borderRadius: '5px',
+  overflow: 'hidden'
 }
 
-const textTimelineContainerTwo = {
+const textContainerTwo = {
   display: 'flex',
   textAlign: 'left',
   alignItems: 'center',
@@ -40,7 +42,8 @@ const textTimelineContainerTwo = {
   margin: '15px 30px 15px 0px',
   backgroundColor: '#eaeaed',
   padding: '50px',
-  borderRadius: '5px'
+  borderRadius: '5px',
+  overflow: 'hidden'
 }
 
 const titleSection = {
@@ -49,27 +52,20 @@ const titleSection = {
   textAlign: 'center'
 }
 
-const titleSection1 = {
+const subtitleSection = {
   fontWeight: '700',
-  maxWidth: '100%',
+  width: '100%',
   overflow: 'hidden',
   color: '#242424'
 }
 
-const textSection1 = {
+const textSection = {
   fontWeight: '200',
   maxWidth: '100%',
   overflow: 'hidden',
   marginTop: '0px',
-  color: '#242424'
-}
-
-const linksContainer = {
-  display: 'flex',
-  flexDirection: 'row',
-  maxHeight: '60px',
-  maxWidth: '60vw',
-  overflow: 'hidden'
+  color: '#242424',
+  maxHeight: '200px'
 }
 
 const linksStyle1 = {
@@ -79,7 +75,9 @@ const linksStyle1 = {
   border: '2px solid white',
   color: 'white',
   fontFamily: "'Roboto', sans-serif",
-  borderRadius: '3px'
+  borderRadius: '3px',
+  maxHeight: '70px',
+  overflow: 'hidden'
 }
 
 const linksStyle2 = {
@@ -89,29 +87,31 @@ const linksStyle2 = {
   border: '2px solid #2d334d',
   color: '#2d334d',
   fontFamily: "'Roboto', sans-serif",
-  borderRadius: '3px'
+  borderRadius: '3px',
+  maxHeight: '70px',
+  overflow: 'hidden'
 }
 
 class ClassicTwoContainer extends Component {
   render () {
     const { userStyle, mode, info, children: optionsBar, openChangeModal } = this.props
     return (
-      <div style={Object.assign({}, timelineContainer, userStyle)}>
+      <div style={Object.assign({}, twoContainer, userStyle)}>
         {optionsBar}
         <h1 style={Object.assign({}, titleSection, info.text5.style)} data-id="text5" onDoubleClick={openChangeModal}>{info.text5.text}</h1>
         <div style={container4options}>
-          <div style={textTimelineContainerOne}>
+          <div style={textContainerOne}>
             <div>
-              <h2 style={Object.assign({}, titleSection1, info.text1.style)} data-id="text1" onDoubleClick={openChangeModal}>{info.text1.text}</h2>
-              <p style={Object.assign({}, textSection1, info.text6.style)} data-id="text6" onDoubleClick={openChangeModal}>{info.text6.text}</p>
-              <LinksListEditable mode={mode} openChangeModal={openChangeModal} info={info} contentStyle={info} containerStyle={linksContainer} linksStyle={linksStyle1}/>
+              <h2 style={Object.assign({}, subtitleSection, info.text1.style)} data-id="text1" onDoubleClick={openChangeModal}>{info.text1.text}</h2>
+              <p style={Object.assign({}, textSection, info.text6.style)} data-id="text6" onDoubleClick={openChangeModal}>{info.text6.text}</p>
+              <LinkEditable contentAttrStyle={info.link1.style} mode={mode} info={info.link1} style={linksStyle1} data-id='link1' onDoubleClick={openChangeModal}/>
             </div>
           </div>
-          <div style={textTimelineContainerTwo}>
+          <div style={textContainerTwo}>
             <div>
-              <h2 style={Object.assign({}, titleSection1, info.text2.style)} data-id="text2" onDoubleClick={openChangeModal}>{info.text2.text}</h2>
-              <p style={Object.assign({}, textSection1, info.text7.style)} data-id="text7" onDoubleClick={openChangeModal}>{info.text7.text}</p>
-              <LinksListEditable mode={mode} openChangeModal={openChangeModal} info={info} contentStyle={info} containerStyle={linksContainer} linksStyle={linksStyle2}/>
+              <h2 style={Object.assign({}, subtitleSection, info.text2.style)} data-id="text2" onDoubleClick={openChangeModal}>{info.text2.text}</h2>
+              <p style={Object.assign({}, textSection, info.text7.style)} data-id="text7" onDoubleClick={openChangeModal}>{info.text7.text}</p>
+              <LinkEditable contentAttrStyle={info.link2.style} mode={mode} info={info.link2} style={linksStyle2} data-id='link2' onDoubleClick={openChangeModal}/>
             </div>
           </div>
         </div>
